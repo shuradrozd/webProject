2fb7997dcbb2457ff5fe1177ac9b7a8b3620b5ae
shuradrozd@mail.ru <bonou2u2>
2022-08-24 11:52:20 +0300

    added check if session closed with error

--- fixtures/session.py
+++ fixtures/session.py
@@ -18,3 +18,23 @@ class Session:
     def logout(self):
         driver = self.app.driver
         driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def ensure_logout(self):
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        driver = self.app.driver
+        return len(driver.find_elements(By.LINK_TEXT, "Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        driver = self.app.driver
+        return driver.find_element_by_xpath('//b[normalize-space()="(admin)"]').text == "(" + username + ")"
+
+    def ensure_login(self, username, password):
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
--- tests/test_delete_first_group.py
+++ tests/test_delete_first_group.py
@@ -1,2 +1,7 @@
+from models.group import Group
+
+
 def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.add_new_group(Group(name="hello"))
     app.group.delete_first_group()
