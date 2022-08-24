f8b5352ea95871e87074486d6813ebd56ae5cebf
shuradrozd@mail.ru <bonou2u2>
2022-08-24 11:55:47 +0300

    refactored modify group functionality

--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -2,4 +2,6 @@ from models.group import Group
 
 
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.add_new_group(Group(name="hello"))
     app.group.modify_first_group(Group(name="New group"))
