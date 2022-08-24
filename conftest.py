e7467be95e3ed346cdeffcf9acc3d29427eb7d14
shuradrozd@mail.ru <bonou2u2>
2022-08-24 09:44:45 +0300

    refactored code to work with scope="session" correctly

--- conftest.py
+++ conftest.py
@@ -17,11 +17,12 @@ def app(request):
             fixture.session.login(username="admin", password="secret")
     return fixture
 
-    @pytest.fixture(scope="session", autouse=True)
-    def stop(request):
-        def fin():
-            fixture.session.logout()
-            fixture.destroy()
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.logout()
+        fixture.destroy()
 
     request.addfinalizer(fin)
     return fixture
