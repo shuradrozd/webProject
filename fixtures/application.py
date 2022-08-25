ce0a2112a3b4fd26338c956a7924d5ea31434ee8
shuradrozd@mail.ru <bonou2u2>
2022-08-25 15:27:01 +0300

    added functionality to work with options (browser_name, base_url) in cmd

--- fixtures/application.py
+++ fixtures/application.py
@@ -1,18 +1,26 @@
 from selenium import webdriver
 
-from fixtures.group import GroupHelper
 from fixtures.contact import ContactHelper
+from fixtures.group import GroupHelper
 from fixtures.session import Session
 
 
 class Application:
-    def __init__(self):
-        self.driver = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.driver = webdriver.Firefox()
+        elif browser == "chrome":
+            self.driver = webdriver.Chrome()
+        elif browser == "edge":
+            self.driver = webdriver.Edge()
+        else:
+            raise ValueError("Unrecognized browser %" % browser)
         self.driver.maximize_window()
-        # self.driver.implicitly_wait(1)
+        # self.driver.implicitly_wait(2)
         self.session = Session(self)
         self.groupHelper = GroupHelper(self)
         self.contactHelper = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -22,7 +30,7 @@ class Application:
             return False
 
     def open_home_page(self):
-        self.driver.get("http://localhost/addressbook/")
+        self.driver.get(self.base_url)
 
     def destroy(self):
         self.driver.quit()
