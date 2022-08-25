dcf752165c4ad1b32957278e1d0f8d062c7c3621
shuradrozd@mail.ru <bonou2u2>
2022-08-25 12:03:33 +0300

    added ddt and generation random values for group name

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,9 +1,24 @@
+import random
+import string
+
+import pytest
+
 from models.group import Group
 
 
-def test_add_group(app):
+def random_string(prefix, maxLen):
+    symbols = string.ascii_letters + string.digits + " " * 2
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxLen))])
+
+
+test_data = [Group(name="")] + [Group(name=random_string("name", 10))
+                                for i in range(5)
+                                ]
+
+
+@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
+def test_add_group(app, group):
     old_groups = app.groupHelper.get_group_list()
-    group = Group(name="test1")
     app.groupHelper.add_new_group(group)
 
     assert len(old_groups) + 1 == app.groupHelper.count()
